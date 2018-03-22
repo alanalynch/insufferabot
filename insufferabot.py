e2dde0e2f0854deff01cab48d45d084cc2e94669
alanalynch <alana.jc.lynch@gmail.com>
2018-03-22 10:16:14 -0400

    minor tweaks and adding TODO

--- insufferabot.py
+++ insufferabot.py
@@ -2,16 +2,16 @@ import markovify
 from twitter_scraper import get_tweets
 
 def combine_tweets():
-    # for some reason 4, 5, and 7 aren't working rn
-    tweets = '\n'.join([t['text'] for t in get_tweets('ericgarland', pages=15)])
+    # TODO: fix this to be less hacky, it randomly chokes for reasons I haven't worked out yet
+    tweets = '\n'.join([t['text'] for t in get_tweets('ericgarland', pages=10)])
     tweets2 = '\n'.join([t['text'] for t in get_tweets('jordanbpeterson', pages=15)])
     tweets3 = '\n'.join([t['text'] for t in get_tweets('nytdavidbrooks', pages=15)])
-    #tweets4 = '\n'.join([t['text'] for t in get_tweets('louisemensch', pages=5)])
-    #tweets5 = '\n'.join([t['text'] for t in get_tweets('drjillstein', pages=5)])
+    tweets4 = '\n'.join([t['text'] for t in get_tweets('bariweiss', pages=3)])
+    tweets5 = '\n'.join([t['text'] for t in get_tweets('ggreenwald', pages=4)])
     tweets6 = '\n'.join([t['text'] for t in get_tweets('keitholbermann', pages=5)])
-    #tweets7 = '\n'.join([t['text'] for t in get_tweets('billmaher', pages=3)])
+    tweets7 = '\n'.join([t['text'] for t in get_tweets('billmaher', pages=1)])
     tweets8 = '\n'.join([t['text'] for t in get_tweets('paulkrugman', pages=5)])
-    combined = tweets + '\n' + tweets2 + '\n' + tweets3 + '\n' + tweets6 + '\n' + tweets8
+    combined = tweets + '\n' + tweets2 + '\n' + tweets3 + '\n' + tweets6 + '\n' + tweets7 + '\n' + tweets8 + '\n' + tweets5 + '\n' + tweets4
 
     return combined
 
@@ -21,5 +21,5 @@ if __name__=='__main__':
     while True:
         print(text_model.make_short_sentence(140))
         response = input('Want another? ')
-        if response == 'y':
+        if lower(response) in ["y", "yes"]:
             continue
